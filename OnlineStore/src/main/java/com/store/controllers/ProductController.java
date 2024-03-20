2d07dd1923493f4afdf96fb52d0c62ab9e75a7fd
ratchevlubo <ratchevlubo@gmail.com>
2024-03-20 16:36:58 +0200

    add products function

--- OnlineStore/src/main/java/com/store/controllers/ProductController.java
+++ OnlineStore/src/main/java/com/store/controllers/ProductController.java
@@ -3,7 +3,11 @@ package com.store.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.store.models.Product;
 import com.store.services.ProductService;
 
 
@@ -19,6 +23,20 @@ public class ProductController {
 		System.out.println(productService.getProducts());
 		model.addAttribute("products", productService.getProducts());
 		
+		return "index.html";
+	}
+	
+	@GetMapping("/addProduct")
+	public String addProduct(Model model)
+	{
+		model.addAttribute("product", new Product());
+		
+		return "addProduct.html";
+	}
+	
+	@PostMapping("/addNew")
+	public String addNew(@Valid Product p, BindingResult bindingResult, Model model)
+	{
 		return "index.html";
 	}
 }
