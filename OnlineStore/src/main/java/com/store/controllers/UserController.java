55733471c951c53af3647cb11a0efbfcc2ee7957
SashoN1913 <72822778+SashoN1913@users.noreply.github.com>
2024-03-21 00:40:08 +0200

    Update User, Add UserController and UserRepository

---
+++ OnlineStore/src/main/java/com/store/controllers/UserController.java
@@ -0,0 +1,32 @@
+package com.store.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.store.models.User;
+import com.store.repository.UserRepository;
+
+@Controller
+public class UserController
+{
+	@Autowired
+	UserRepository userRepository;
+	
+	@GetMapping("/registrations")
+	public String getRegistrationPage(@ModelAttribute("user") User user)
+	{
+		return "registrations";
+	}
+	
+	@PostMapping("/registrations")
+	public String saveUser(@ModelAttribute("user") User user, Model model)
+	{
+		userRepository.save(user);
+		model.addAttribute("message", "Submitted Succesuflu");
+		return "loginResults";
+	}
+}
--- OnlineStore/src/main/java/com/store/models/User.java
+++ OnlineStore/src/main/java/com/store/models/User.java
@@ -1,7 +1,17 @@
 package com.store.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User
 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
 	private String firstName;
 	private String lastName;
 	private String phone;
