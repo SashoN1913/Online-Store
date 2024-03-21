bd6ccafd11aaccc35eb81a27496fae1e7e0e726b
SashoN1913 <72822778+SashoN1913@users.noreply.github.com>
2024-03-22 00:20:56 +0200

    findByEmail in UserRepository

--- OnlineStore/src/main/java/com/store/repository/UserRepository.java
+++ OnlineStore/src/main/java/com/store/repository/UserRepository.java
@@ -1,12 +1,17 @@
 package com.store.repository;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.store.models.User;
+import java.util.List;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>
 {
+	@Query("SELECT u FROM User WHERE u.email = ?1")
+	User findByEmail(String email);
 	
 }
