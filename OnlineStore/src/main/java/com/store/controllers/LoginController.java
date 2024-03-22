a1ba38585cae4518940ae3630907532e21948f06
SashoN1913 <72822778+SashoN1913@users.noreply.github.com>
2024-03-23 00:01:50 +0200

    LoginForm working almost

--- OnlineStore/src/main/java/com/store/controllers/LoginController.java
+++ OnlineStore/src/main/java/com/store/controllers/LoginController.java
@@ -4,30 +4,40 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.store.models.LoginModel;
 
 @Controller
-@RequestMapping("/login")
 public class LoginController
 {
 
-	@GetMapping("/")
+	@GetMapping("/login")
 	public String displayLoginForm(Model model)
 	{
 		model.addAttribute("loginModel", new LoginModel());
 		return "login.html";
 	}
 
-	@PostMapping("/processLogin")
-	public String processLogin(LoginModel loginModel, Model model)
+	@PostMapping("/login-error")
+	public ModelAndView failLogin(@ModelAttribute("email") String email)
 	{
-		BCryptPasswordEncoder b = new BCryptPasswordEncoder();
-		model.addAttribute("email", loginModel.getEmail());
-		model.addAttribute("password", loginModel.getPassword());
-		System.out.println(loginModel.getEmail());
-		return "loginResults.html";
+		ModelAndView modelAndView = new ModelAndView("login");
+		modelAndView.addObject("email", email);
+		modelAndView.addObject("bad_credentials", "true");
+
+		return modelAndView;
 	}
+
+	/*
+	 * @PostMapping("/processLogin") public String processLogin(LoginModel
+	 * loginModel, Model model) { BCryptPasswordEncoder b = new
+	 * BCryptPasswordEncoder(); model.addAttribute("email", loginModel.getEmail());
+	 * model.addAttribute("password", loginModel.getPassword());
+	 * //System.out.println(loginModel.getEmail()); return "loginResults.html"; }
+	 */
 }
--- OnlineStore/src/main/java/com/store/models/SecurityConfig.java
+++ OnlineStore/src/main/java/com/store/models/SecurityConfig.java
@@ -1,15 +1,10 @@
 package com.store.models;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.store.repository.UserRepository;
-import com.store.services.CustomUserDetailsService;
 import com.store.services.UserService;
 
 @Configuration
@@ -28,66 +22,67 @@ public class SecurityConfig
 
 	@Autowired
 	private CustomAuthentication authentication;
-	/*
-	 * public AuthenticationProvider daoAuthenticationProvider() {
-	 * DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-	 * provider.setPasswordEncoder(passwordEncoder()); //
-	 * provider.setUserDetailsService(this.databaseUserDetailsService); return
-	 * provider; }
-	 */
 
-	/*
-	 * @Autowired private DataSource dataSource;
-	 * 
-	 * @Autowired UserRepository userDetailsService;
-	 * 
-	 * @Bean UserDetailsService userDetailsService() { return new
-	 * CustomUserDetailsService(); }
-	 * 
-	 * public DaoAuthenticationProvider authenticationProvider() {
-	 * DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-	 * authProvider.setUserDetailsService(userDetailsService());
-	 * authProvider.setPasswordEncoder(passwordEncoder());
-	 * 
-	 * return authProvider; }
-	 */
-	@Primary
-	@Bean
-	public AuthenticationManagerBuilder confi(AuthenticationManagerBuilder auth) throws Exception
-	{
-		auth.authenticationProvider(authentication);
-		return auth;
-	}
+    /*
+     * public AuthenticationProvider daoAuthenticationProvider() {
+     * DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+     * provider.setPasswordEncoder(passwordEncoder()); //
+     * provider.setUserDetailsService(this.databaseUserDetailsService); return
+     * provider; }
+     */
 
+    /*
+     * @Autowired private DataSource dataSource;
+     * 
+     * @Autowired UserRepository userDetailsService;
+     * 
+     * @Bean UserDetailsService userDetailsService() { return new
+     * CustomUserDetailsService(); }
+     * 
+     * public DaoAuthenticationProvider authenticationProvider() {
+     * DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+     * authProvider.setUserDetailsService(userDetailsService());
+     * authProvider.setPasswordEncoder(passwordEncoder());
+     * 
+     * return authProvider; }
+     */
 	/*
-	 * @Bean AuthenticationManager authenticationManager(AuthenticationConfiguration
-	 * configuration) throws Exception { return
-	 * configuration.getAuthenticationManager(); }
-	 */
-
-	/*
-	 * @Autowired public void configureGlobal(AuthenticationManagerBuilder auth)
-	 * throws Exception { auth.userDetailsService(CustomUserDetails());
+	 * @Primary
 	 * 
-	 * auth.jdbcAuthentication()
-	 * .dataSource(dataSource).passwordEncoder(passwordEncoder());
-	 * 
+	 * @Bean AuthenticationManagerBuilder confi(AuthenticationManagerBuilder auth)
+	 * throws Exception { auth.authenticationProvider(authentication); return auth;
 	 * }
 	 */
 
-	/*
-	 * @Bean public UserDetailsService userDetailsManager() {
-	 * 
-	 * UserDetails user =
-	 * org.springframework.security.core.userdetails.User.builder()
-	 * .username("susan") .password("{noop}test123") .roles("EMPLOYEE", "MANAGER",
-	 * "ADMIN") .build();
-	 * 
-	 * return new InMemoryUserDetailsManager(user); }
-	 */
+    /*
+     * @Bean AuthenticationManager authenticationManager(AuthenticationConfiguration
+     * configuration) throws Exception { return
+     * configuration.getAuthenticationManager(); }
+     */
 
-	@Bean
-	public UserDetailsService userDetailsService(UserRepository userRepository)
+    /*
+     * @Autowired public void configureGlobal(AuthenticationManagerBuilder auth)
+     * throws Exception { auth.userDetailsService(CustomUserDetails());
+     * 
+     * auth.jdbcAuthentication()
+     * .dataSource(dataSource).passwordEncoder(passwordEncoder());
+     * 
+     * }
+     */
+
+    /*
+     * @Bean public UserDetailsService userDetailsManager() {
+     * 
+     * UserDetails user =
+     * org.springframework.security.core.userdetails.User.builder()
+     * .username("susan") .password("{noop}test123") .roles("EMPLOYEE", "MANAGER",
+     * "ADMIN") .build();
+     * 
+     * return new InMemoryUserDetailsManager(user); }
+     */
+
+    @Bean
+    UserDetailsService userDetailsService(UserRepository userRepository)
 	{
 		return new UserService(userRepository);
 	}
@@ -101,10 +96,10 @@ public class SecurityConfig
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception
 	{
-		http.authorizeHttpRequests((requests) -> requests.requestMatchers("/product", "/store").permitAll()
+		http.authorizeHttpRequests((requests) -> requests.requestMatchers("/product", "/").permitAll()
 				.anyRequest().authenticated())
-				.formLogin((form) -> form.usernameParameter("email").passwordParameter("password")
-						.defaultSuccessUrl("/loginResults").permitAll())
+				.formLogin((form) -> form.loginPage("/login").usernameParameter("email").passwordParameter("password")
+						.defaultSuccessUrl("/index").permitAll())
 				.logout((logout) -> logout.permitAll());
 
 		return http.build();
