06a70bdd4ee4ff4c7f79b15ac0cbb91fdcf9119c
ratchevlubo <ratchevlubo@gmail.com>
2024-04-02 20:02:38 +0300

    securityConfing fix

--- OnlineStore/src/main/java/com/store/config/SecurityConfig.java
+++ OnlineStore/src/main/java/com/store/config/SecurityConfig.java
@@ -6,19 +6,47 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-public class SecurityConfig
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurity
 {
-//	@Bean
-//	@Primary
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        return http
-//            .authorizeHttpRequests(auth -> {
-//            	auth.requestMatchers("/**").permitAll();
-//            	//auth.anyRequest().authenticated();
-//            })
-//            .build();
-//    }
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+    private static final String[] PUBLIC_MATCHERS = {
+            "/css/**",
+            "/js/**",
+            "/image/**",
+            "/",
+            "/login/**",
+            "/store/**",
+            "/register/**"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+        http.authorizeHttpRequests(
+                        (requests) ->
+                                .requestMatchers(PUBLIC_MATCHERS).permitAll()
+                                .requestMatchers("/admin/**").hasRole("ADMIN"))
+                .formLogin(
+                        (form) -> form.loginPage("/login").loginProcessingUrl("/login").defaultSuccessUrl("/users").permitAll())
+                .logout((logout) -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll());
+
+        return http.build();
+    }
+
+	@Bean
+	public static PasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
+	{
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
 }
