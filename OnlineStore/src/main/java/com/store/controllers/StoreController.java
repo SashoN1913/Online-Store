4cae305703bf11935a3e61496078dc6cd9d2d7a4
ratchevlubo@gmail.com <ratchevlubo@gmail.com>
2024-04-10 12:37:38 +0300

    log info when someone access the store

--- OnlineStore/src/main/java/com/store/controllers/StoreController.java
+++ OnlineStore/src/main/java/com/store/controllers/StoreController.java
@@ -1,5 +1,7 @@
 package com.store.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,10 +18,14 @@ public class StoreController
 {
 	@Autowired
 	private ProductService productService;
-	
+
+	Logger logger = LoggerFactory.getLogger(StoreController.class);
+
+
 	@GetMapping("/store")
 	public String view(Model model)
 	{
+		logger.info("someone access store");
 		model.addAttribute("products", productService.getProducts());
 		return "store.html";
 	}
